39062c6c6a38b0e2ba6958090afbb0ca3ff9d03d
Yinka Gold <yinkagolden@gmail.com>
2018-03-09 17:50:11 +0200

    Starting JavaScript, basic function

---
+++ course.js
@@ -0,0 +1,18 @@
+<!doctype html>
+	
+	<html>
+
+	<head>
+
+		<meta charset= "utf-8">
+		<title> Introduction to JavaScript</title>
+
+	</head>
+
+	<body>
+	
+		<script src = "../js/course.js"></script>
+		
+	</body>
+
+</html>
---
+++ prompt.js
@@ -0,0 +1,2 @@
+var name = prompt("Enter your name :");
+alert ("Hello , " + name );
