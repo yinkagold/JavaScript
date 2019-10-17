3d895d87617e2a1d77784ab94a6b0baca0e4a6c1
Yinka Gold <yinkagolden@gmail.com>
2019-10-17 13:02:02 +0300

    consuming an API with cross origin

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-//get max
-// const numbers = [1, 2, 3, 4];
+/*
+var request = new XMLHttpRequest();
+request.open("GET", "data.json", true);
+request.onload = function() {
+  var data = JSON.parse(this.response);
 
-// const max = getMax([1, 4]);
+  for (var i = 0; i < data.length; i++) {
+    console.log(data[i].name + "is a " + data[i].race);
+  }
+};
+request.send();
 
-// console.log(max);
+//using the fecth
 
-// function getMax(array) {
-//   if (array.length === 0) return undefined; // check if array is empty
+fetch("./data.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {});
 
-//   //   let max = array[0];
-//   //   for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
-//   //   return max;
+// using Jquery
+$(document).ready(function() {
+  var data;
+  $.ajax({
+    dataType: "json",
+    url: "data.json",
+    data: data,
+    success: function(data) {
+      data.map(elem => {
+        console.log(elem);
+      });
+      //data.forEach(elem => {
+      //console.log(elem);
+    }
+  });
+});
+*/
+const app = document.getElementById("root");
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+app.appendChild(container);
 
-//   return array.reduce((a, b) => (a > b ? a : b));
-// }
-
-// movies
-const movies = [
-  { title: "a", year: 2018, rating: 4.5 },
-  { title: "b", year: 2018, rating: 4.7 },
-  { title: "c", year: 2018, rating: 3 },
-  { title: "d", year: 2017, rating: 4.5 }
-];
-
-const title = movies
-  .filter(m => m.year === 2018 && m.rating >= 4)
-  .sort((a, b) => a.rating - b.rating)
-  .reverse()
-  .map(m => m.title);
-console.log(title);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://bwa-2019-2020.herokuapp.com/v1/trips", true);
+xhr.setRequestHeader("X-API-Key", "bwa_2019-2020");
+xhr.onload = function() {
+  var data = JSON.parse(this.response);
+  data.forEach(trips => {
+    //console.log(trips.scooter_id);
+  });
+  console.log(xhr.responseText);
+};
+xhr.send();
