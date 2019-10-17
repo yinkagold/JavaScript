19fff292a90237d0a07371e3b105d56a275ba2d7
Yinka Gold <yinkagolden@gmail.com>
2019-10-17 12:55:44 +0300

    consuming an API using XMLHttpRequest

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ var planet = {
 //planet.name = "Peter";
 //document.write("<br />" + planet.name)
 
-//var z = planet; 
+//var z = planet;
 //document.write(z.name);
 
-// adding to existing object literals 
+// adding to existing object literals
 /*
 if (typeof planet.defence == "undefined"){
 planet.defence = "Earth";
@@ -60,16 +60,50 @@ for (i in planet){
 	document.write("<br />" + i + ":" + planet[i]);
 }*/
 
-function car(make, model, year){ // constructor 
-	this.make = make;
-	this.model = model;
-	this.year = year;
-	
-}
+const app = document.getElementById("root");
+const logo = document.createElement("img");
+logo.src = "logo.png";
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+app.appendChild(logo);
+app.appendChild(container);
 
-var myCar = new car("Honda", "Accord", 1996);
-var newCar = new car("Volks", "Golf", 2004);
+var request = new XMLHttpRequest();
 
-for (i in myCar){
-	document.write("<br />" + myCar[i]);
-}
+request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+
+request.onload = function() {
+  var data = JSON.parse(this.response);
+
+  //checking for error within the range of 200-300
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      //creating a div with a card class
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      // create h1 and set the text content to the films title
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
+
+      // create a p and set the text content to the films description
+      const p = document.createElement("p");
+      movie.description = movie.description.substring(0, 300); // Limit to 300 chars
+      p.textContent = `${movie.description}...`; // End with an ellipses
+
+      //Append the cards to the container element
+      container.appendChild(card);
+      // Each card will contain an h1 and p
+      card.appendChild(h1);
+      card.appendChild(p);
+      //console.log(movie.title);
+    });
+  } else {
+    const errorMessage = document.createElement("marquee");
+    errorMessage.textContent = `Its not workfing!`;
+    app.appendChild(errorMessage);
+    //console.log("error");
+  }
+};
+
+request.send();
